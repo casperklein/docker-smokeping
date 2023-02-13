f57591b980ca3214fdd9bd95eb6f10cbf15db46d
casperklein <casperklein@users.noreply.github.com>
2023-02-13 23:22:50 +0100

    also consider /var/lib/smokeping

--- rootfs/run.sh
+++ rootfs/run.sh
@@ -2,11 +2,16 @@
 
 set -ueo pipefail
 
-mkdir -p /var/lib/smokeping/__cgi
-
-# directory empty? copy default config
+# directory /etc/smokeping/ empty? copy default config
 if [ -z "$(ls -A /etc/smokeping/)" ]; then
 	cp -a /etc/.smokeping/* /etc/smokeping/
+fi
+
+# mkdir -p /var/lib/smokeping/__cgi
+
+# directory /var/lib/smokeping/ empty? copy default data
+if [ -z "$(ls -A /var/lib/smokeping/)" ]; then
+	cp -a /var/lib/.smokeping/* /var/lib/smokeping/
 fi
 
 # start smokeping
